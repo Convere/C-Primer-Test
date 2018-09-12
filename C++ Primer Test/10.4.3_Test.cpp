9deb125bfe980b6db9eaa6f8dea421d9c43c8f66
1802802 <35828295+1802802@users.noreply.github.com>
2018-09-12 10:43:50 +0800

    9.12

---
+++ C++ Primer Test/10.4.3_Test.cpp
@@ -0,0 +1,50 @@
+/*#include<iostream>  
+#include<string>  
+#include<vector>  
+#include<algorithm>  
+#include<numeric>  
+#include<functional>
+#include<iterator>
+#include<list>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+/*int main1()  //Á·Ï°10.34
+{
+	vector<string> text{ "i","can","fly","fly","on","the","sky!" };
+	for (auto iter=text.crbegin(); iter != text.crend(); ++iter)
+	{
+		cout << *iter << endl;
+	}
+	return 0;
+}
+
+int main2()  //Á·Ï°10.35
+{
+	vector<string> text{ "i","can","fly","fly","on","the","sky!" };
+	for (auto iter = text.end(); iter != text.begin(); iter--)
+	{
+		cout << *(iter - 1) << endl;
+	}
+	return 0;
+}
+
+int main3() //Á·Ï°10.36
+{
+	list<int> text{ 1,2,3,0,1,0,5,0,8 };
+	auto temp = find(text.rbegin(), text.rend(), 0);
+	cout << &*temp << endl;
+	return 0;
+}
+
+int main4()  //Á·Ï°10.37
+{
+	vector<int> text{ 1,2,3,0,1,0,5,0,8,10 };
+	list<int> text2;
+	copy(text.rbegin() + 2, text.rend() - 3,back_inserter(text2));  //·¶Î§begin±Õ£¬end¿ª
+	for (auto &s : text2)
+		cout << s << endl;
+}*/
+	
---
+++ C++ Primer Test/10.6_Test.cpp
@@ -0,0 +1,26 @@
+#include<iostream>  
+#include<string>  
+#include<vector>  
+#include<algorithm>  
+#include<numeric>  
+#include<functional>
+#include<iterator>
+#include<list>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+int main()  //练习10.42
+{
+	list<string> words{ "my","name","is","amazing","name" };
+	for (auto &s : words)
+		cout << s << endl;
+	for (auto beg = words.begin(); beg != words.end(); beg++)
+		cout << "原句为：" << *beg << endl;
+	words.sort();
+	words.unique();
+	//doit(words);
+	for (auto beg = words.begin(); beg != words.end(); beg++)
+		cout << "修改为：" << *beg << endl;
+}
